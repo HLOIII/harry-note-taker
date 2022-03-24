afccf718ba75f10db122d8cf5d6b9852b7efd56c
Harry <chairmanharryogletree@gmail.com>
2022-03-24 01:39:53 -0400

    <fixes>

--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -1,14 +1,14 @@
 // dependencies
 // start an instance of Router
 const router = require('express').Router();
-const { createNewNotes } = require('../../lib/notes');
+const {createNewNotes} = require('../../lib/notes');
 const fs = require('fs');
 const path = require('path');
 
 // for id creation
 const shortid = require('shortid');
 
-let { notes } = require('../../db/db.json');
+let {notes} = require('../../db/db.json');
 
 // route to get saved notes
 router.get('/notes', (req, res) => {
@@ -27,7 +27,7 @@ router.post('/notes', (req, res) => {
 // delete route
 router.delete('/notes/:id', (req, res) => {
     notes = notes.filter(note => note.id !== req.params.id);
-    fs.writeFileSync(path.join(__dirname, '../../db/db.json'), JSON.stringify({ notes }, null, 2));
+    fs.writeFileSync(path.join(__dirname, '../../db/db.json'), JSON.stringify({notes}, null, 2));
     res.json(notes);
 });
 
--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const path = require('path');
 const express = require('express');
 
 // requiring data
-const { notes } = require('./db/db.json');
+const {notes} = require('./db/db.json');
 
 // sets an environment variable for port
 const PORT = process.env.PORT || 3001;
@@ -16,7 +16,7 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 
 // parse incoming string or array data
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({extended: true}));
 //parse incoming JSON data
 app.use(express.json());
 // instruct the server to make certain files readily available
